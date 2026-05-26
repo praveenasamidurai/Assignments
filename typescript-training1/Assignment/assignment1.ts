c5deac4acfc426da44d4b56cf4016654337fb898
Praveena Samidurai <praveenasamidurai@gmail.com>
2026-05-26 21:06:10 +0530

    assignment1

---
+++ typescript-training1/Assignment/assignment1.ts
@@ -0,0 +1,3 @@
+//1.Temperature of a city in degrees Celsius: 25.5
+let temperature: number = 25.5;
+console.log(`Temperature of a city in degrees Celsius: ${temperature}`);
---
+++ typescript-training1/language-basics/data-type.ts
@@ -0,0 +1,2 @@
+let empid:number = 122344;
+console.log(empid);
