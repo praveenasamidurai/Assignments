182b40b3d611c1707fdb0ea48f4b45e85f00a2ec
Praveena Samidurai <praveenasamidurai@gmail.com>
2026-05-27 12:58:59 +0530

    Assignment 1

--- typescript-training1/Assignment/assignment1.ts
+++ typescript-training1/Assignment/assignment1.ts
@@ -11,23 +11,69 @@ let phoneNumber: string = "123-456-7890";
 console.log (`Person's phone number:${phoneNumber}`);
 
 //4. Amount of money in a customer's bank account: 1000.50
+let money: number = 1000.50;
+console.log (`Amount of money in a customer's bank account:${money}`);
+
 //5. Person's email address: "john.doe@example.com"
+let email: string = "john.doe@example.com";
+console.log (`Person's email address:${email}`);
+
 //6. Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+let coordinates: number [] = [37.7749, -122.4194];
+console.log ("Coordinates of a location:" +coordinates[0] +","+coordinates[1]);
+
 //7. Person's marital status: true or false
+let maritalstatus: boolean = true;
+console.log (`Person's marital status:${maritalstatus}`);
+
 //8. Person's occupation: "Software Engineer"
+let occupation: string = "Software Engineer";
+console.log (`Person's occupation:${occupation}`);
+
 //9. Person's favourite colour: "Blue"
+let favColor: string = "Blue";
+console.log (`Person's favourite colour:${favColor}`);
 
 //10.Current year: 2023
-let currentYear: number=new Date().getFullYear();
+let currentYear: number=2023;
 console.log (`current Year :${currentYear}`)
 
 //11.Number of followers on a social media platform: 1,000,000
+let NoOfFollowers: string = "1,000,000";
+console.log (`Number of followers on a social media platform:${NoOfFollowers}`);
+
 //12.Rating of a movie: 7.5
+let movieRating: number = 7.5;
+console.log (`Rating of a movie:${movieRating}`);
+
 //13.Person's blood type: 'A'
+let bloodType: string = "A";
+console.log (`Person's blood type: ${bloodType}`);
+
 //14.Title of a book: "To Kill a Mockingbird"
+let titleOfTheBook: string = "To Kill a Mockingbird";
+console.log (`Title of a book:${titleOfTheBook}`);
+
 //15.Number of employees in a company: 500
+let noOfEmployees: number = 500;
+console.log (`Number of employees in a company:${noOfEmployees}`);
+
 //16.Time of an event: 2:30 PM
+let eventTime: string = "2:30 PM";
+console.log (`Time of an event:${eventTime}`);
+
 //17.Name of a country: "United States"
+let countryName: string = "United States";
+console.log (`Name of a country: ${countryName}`);
+
 //18.Person's eye color: "Brown"
+let eyeColor: string = "Brown";
+console.log (`Person's eye color:${eyeColor}`);
+
 //19.Person's birthplace: "New York City"
+let birthplace: string = "New York City";
+console.log (`Person's birthplace: ${birthplace}`);
+
 //20. Distance between two cities: 200.5
+let distance: number = 200.5;
+console.log (`Distance between two cities:${distance}`);
