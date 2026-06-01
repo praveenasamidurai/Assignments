f44cfa8d7d5fb8e37f6dca1cdfa85690f00d10fc
Praveena Samidurai <praveenasamidurai@gmail.com>
2026-06-01 21:14:44 +0530

    Assignment 1

--- typescript-training1/Assignment/assignment3.ts
+++ typescript-training1/Assignment/assignment3.ts
@@ -3,14 +3,14 @@ let studentsmark:number[] = [75, 80, 82];
 let bonusmark:number[] = studentsmark.map(studentmark=>studentmark+10);
 
 function addmarks(studentsmark:number[]): void{
-let bonusmark = studentsmark.map(studentsmark=>studentsmark+10);
+let bonusmark : number[]= studentsmark.map(studentsmark=>studentsmark+10);
 console.log("Bonus mark:"+bonusmark);
 }
 
 function avgmarks(bonusmark:number[]): number{
-let sum =0;
-for(let i=0; i<bonusmark.length; i++){
-     sum +=bonusmark[i];
+let sum:number =0;
+for(let i:number =0; i<bonusmark.length; i++){
+     sum +=bonusmark[i]!;
 }
 
 let average= sum/bonusmark.length;
---
+++ typescript-training1/Assignment/assignment4.ts
@@ -0,0 +1,5 @@
+let transactionAmt:number[] = [50000,-2000,3000,-15000.-200,-300,4000,-3000];
+
+for (let i=0;i>transactionAmt.length;i++)
+
+if(transactionAmt>10000||transactionAmt<10000)
